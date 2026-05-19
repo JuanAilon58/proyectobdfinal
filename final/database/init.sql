a9ae18aeb4c1560806437327025485712e67967a
JuanAilon <jailono@miumg.edu.gt>
2026-05-18 21:45:52 -0600

    conexion a base de datos

--- final/database/init.sql
+++ final/database/init.sql
@@ -5,6 +5,7 @@ CREATE TABLE product (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price NUMERIC(10,2) NOT NULL,
+    stock INT DEFAULT 0,
     active BOOLEAN DEFAULT true,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
